0609652d608d0003fce22a587be9a7c049eb7551
Sweetdumplings <linmx0130@163.com>
2012-07-12 22:48:28 +0800

    add define to MTW's header file

--- MenuTabWidget/pQMenuTabWidget.h
+++ MenuTabWidget/pQMenuTabWidget.h
@@ -22,6 +22,10 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 */
+
+#ifndef PQMENUTABWIDGET_H
+#define PQMENUTABWIDGET_H
+
 #include <QPushButton>
 #include <QMenu>
 #include <QStackedLayout>
@@ -77,4 +81,4 @@ class pQMenuTabWidget:public QWidget
 		QMenu *menuObject();
 		QPushButton *menuButton();
 };
-
+#endif
